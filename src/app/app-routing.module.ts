036f402af35b1e739dcaba4197c6b831af0e9d23
stephanogiuseppe <stephano.grp@gmail.com>
2018-11-03 02:34:06 -0300

    invalid url configuration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, UrlSerializer } from '@angular/router';
 
-const routes: Routes = [];
+import { AboutComponent } from './pages/about/about.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ErrorPageComponent } from './layout/error-page/error-page.component';
+
+const routes: Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'invalid-url', component: ErrorPageComponent },
+    { path: '', pathMatch: 'full', redirectTo: 'about' }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [
+        RouterModule.forRoot(routes, {
+            scrollPositionRestoration: 'enabled',
+            malformedUriErrorHandler: (error: URIError, urlSerializer: UrlSerializer, url: string) => {
+                // console.log(error); console.log(url);
+                return urlSerializer.parse('/invalid-url');
+            }
+        }),
+    ],
+    exports: [
+        RouterModule,
+    ]
 })
 export class AppRoutingModule { }
