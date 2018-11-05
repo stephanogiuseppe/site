434a6f2a51c1102db137ab1ff2db5af6b5d737a6
stephanogiuseppe <stephano.grp@gmail.com>
2018-11-05 01:36:41 -0200

    update app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,47 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ApolloModule, Apollo } from 'apollo-angular';
+import { HttpLinkModule, HttpLink } from 'apollo-angular-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-// import { AboutComponent } from './pages/about/about.component';
 import { ErrorPageComponent } from './layout/error-page/error-page.component';
 import { HomeComponent } from './pages/home/home.component';
-import { AboutModule } from './pages/about/about.module';
+// import { AboutModule } from './pages/about/about.module'; // lazy load
+import { StatusService } from './shared/services/status.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    // AboutComponent,
     ErrorPageComponent,
     HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     BrowserAnimationsModule,
-    AboutModule
+    HttpClientModule,
+    ApolloModule,
+    HttpLinkModule,
+    AppRoutingModule,
+    // AboutModule
   ],
-  providers: [],
+  providers: [
+    StatusService
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+    constructor(
+        apollo: Apollo,
+        httpLink: HttpLink
+    ) {
+        apollo.create({
+            link: httpLink.create({ uri: 'https://api.github.com/graphql' }),
+            cache: new InMemoryCache()
+        });
+    }
+}
